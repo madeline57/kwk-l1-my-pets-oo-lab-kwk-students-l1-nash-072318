7e962958d9e324f2119371c304f00be035f80459
madeline57 <maddiejobeth@aol.com>
2018-07-26 21:14:29 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,14 @@
 class Cat
-def initialize(name)
+def initialize(name, mood)
 @name = name
+@mood = mood
 end
-
+def mood
+  @mood
+end
 def name 
 @name 
 end 
 end
 
-crookshanks = Cat.new("crookshanks")
+crookshanks = Cat.new("crookshanks", "nervous")
