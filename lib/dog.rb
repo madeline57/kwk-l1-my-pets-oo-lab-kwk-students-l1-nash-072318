add5bd419c696dc10206a1efe832b4b8ba0f9843
madeline57 <maddiejobeth@aol.com>
2018-07-26 21:23:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
 def initialize(name)
 @name = name
-#@mood = "nervous"
+@mood = "nervous"
 end
-#def mood=(new_mood)
- # @mood=new_mood
-#end
+def mood=(new_mood)
+  @mood=new_mood
+end
 def name 
 @name 
 end 
-#def mood 
-  #@mood
-#end 
+def mood 
+  @mood
+end 
 end
  daisy = Dog.new("daisy")
